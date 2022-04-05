660b60d4cf6bf69eb51ea583846a78771c57b2a2
ayahamza123 <eya.hamza@insat.ucar.tn>
2022-04-05 12:40:25 +0100

    add register

---
+++ serverRegister/src/main/java/server/register/serverRegister/ServerRegisterApplication.java
@@ -0,0 +1,13 @@
+package server.register.serverRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerRegisterApplication.class, args);
+	}
+
+}
